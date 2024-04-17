7d2a823ae176f7299ba32bb80fc75e89e19aab64
Christopher Patrick Fair <christopherpfair@comcast.net>
2024-04-16 22:39:38 -0500

    Configured copilot

--- lua/community.lua
+++ lua/community.lua
@@ -6,6 +6,21 @@
 return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.colorscheme.everforest", enabled = true },
   { import = "astrocommunity.completion.copilot-lua" },
-  -- import/override with your plugins folder
+  {
+    "copilot.lua",
+    opts = {
+      suggestion = {
+        keymap = {
+          accept = "C-l>",
+          accept_word = false,
+          accept_line = false,
+          next = "<C-.>",
+          prev = "<C-,>",
+          dismiss = "<C-/>",
+        },
+      },
+    },
+  },
 }
