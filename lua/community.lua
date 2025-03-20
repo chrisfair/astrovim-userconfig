10b526ce977b9809a5736048434ccaa2777fe814
Christopher Patrick Fair <chris.fair@novatechautomation.com>
2025-03-20 14:53:31 -0500

    Updated to AstroNVim 5
    Signed-off-by: Christopher Patrick Fair <chris.fair@novatechautomation.com>

--- lua/community.lua
+++ lua/community.lua
@@ -1,3 +1,5 @@
+if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+
 -- AstroCommunity: import any community modules here
 -- We import this file in `lazy_setup.lua` before the `plugins/` folder.
 -- This guarantees that the specs are processed before any user plugins.
@@ -6,24 +8,5 @@
 return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.lua" },
-  { import = "astrocommunity.pack.go" },
-  { import = "astrocommunity.pack.python" },
-  { import = "astrocommunity.pack.zig" },
-
-  { import = "astrocommunity.completion.copilot-lua" },
-  {
-    "copilot.lua",
-    opts = {
-      suggestion = {
-        keymap = {
-          accept = "<C-l>",
-          accept_word = false,
-          accept_line = false,
-          next = "<C-.>",
-          prev = "<C-,>",
-          dismiss = "<C-/>",
-        },
-      },
-    },
-  },
+  -- import/override with your plugins folder
 }
