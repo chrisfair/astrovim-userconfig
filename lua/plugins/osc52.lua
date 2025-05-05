590be72bb54c707dd768b6cb93f9426c77355848
Christopher Fair <christopherpatrickfair@gmail.com>
2025-05-05 12:48:02 -0500

    Adds the ability to copy from vim to clipboard
    Signed-off-by: Christopher Fair <christopherpatrickfair@gmail.com>

---
+++ lua/plugins/osc52.lua
@@ -0,0 +1,20 @@
+ {
+    "ojroques/nvim-osc52",
+    config = function()
+      require("osc52").setup{
+        max_length = 0, -- Max length of text to copy
+        silent = false, -- Don't print message when copying
+        trim = true, -- Trim text to fit in the clipboard
+        silent = false, -- Don't print message when copying
+        tmux_passthrough = true; -- Use tmux passthrough
+      }
+
+      local function copy()
+        if vim.v.event.operator == "y" and vim.v.event.regname == "" then
+          require("osc52").copy_register("")
+            end
+      end
+      vim.api.nvim_create_autocmd("TextYankPost", { callback = copy })
+    end,
+  },
+}
