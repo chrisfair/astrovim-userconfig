808ec38b48b0f84fb5d96327552b5ada22d33bd1
Christopher Patrick Fair <christopherpatrickfair@gmail.com>
2026-05-11 07:56:04 -0500

    Removed github copilot from config
    Signed-off-by: Christopher Patrick Fair <christopherpatrickfair@gmail.com>

--- lua/community.lua
+++ lua/community.lua
@@ -8,22 +8,5 @@ return {
   { import = "astrocommunity.pack.lua" },
   { import = "astrocommunity.pack.go" },
   { import = "astrocommunity.pack.python" },
-  { import = "astrocommunity.pack.zig" },
-
-  { import = "astrocommunity.completion.copilot-lua" },
-  {
-    "copilot.lua",
-    opts = {
-      suggestion = {
-        keymap = {
-          accept = "<C-l>",
-          accept_word = false,
-          accept_line = false,
-          next = "<C-.>",
-          prev = "<C-,>",
-          dismiss = "<C-/>",
-        },
-      },
-    },
-  },
+  { import = "astrocommunity.pack.zig" }
 }
